cdfbbdb3ae622faed7f1d628461b24a742ee0a51
Yaroslav Halynskyi <125606535+Yaroslav65@users.noreply.github.com>
2024-05-19 22:27:01 +0200

    react_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+const Married = ({ person }) => {
+  if (person.isMarried === false) {
+    return <p>I am not married</p>;
+  }
+
+  if (person.sex === 'f') {
+    return <p>{person.partnerName} is my husband</p>;
+  }
+
+  return <p>{person.partnerName} is my wife</p>;
+};
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+    {person.age !== undefined ? (
+      <p className="Person__age">
+        <p>{`I am ${person.age}`}</p>
+      </p>
+    ) : null}
+    <p className="Person__partner">
+      <Married person={person} />
+    </p>
+  </section>
+);
